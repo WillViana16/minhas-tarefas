d3a1ab058e78708cdaa2b6f3e209118cd7770383
Willian <willianvia1@gmail.com>
2025-04-23 21:56:26 -0300

    criando projeto to-do list

---
+++ src/pages/Cadastro/index.tsx
@@ -0,0 +1,11 @@
+import BarraLateral from '../../containers/BarraLateral'
+import Formulario from '../../containers/Formularios'
+
+const Cadastro = () => (
+  <>
+    <BarraLateral mostrarFiltros={false} />
+    <Formulario />
+  </>
+)
+
+export default Cadastro
